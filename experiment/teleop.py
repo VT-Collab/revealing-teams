1c8babef5502208e9e5861dc67ce898476e9f811
soheil-h <habibian@vt.edu>
2021-08-26 13:36:41 -0400

    team

---
+++ experiment/teleop.py
@@ -0,0 +1,243 @@
+import socket
+import time
+import numpy as np
+import pickle
+import pygame
+import sys
+import rospy
+import actionlib
+
+
+from control_msgs.msg import (
+    FollowJointTrajectoryAction,
+    FollowJointTrajectoryGoal,
+    GripperCommandAction,
+    GripperCommandGoal,
+    GripperCommand
+)
+from trajectory_msgs.msg import (
+    JointTrajectoryPoint
+)
+from sensor_msgs.msg import (
+    JointState
+)
+from geometry_msgs.msg import(
+    TwistStamped,
+    Twist
+)
+
+
+HOME_POSITION = [0.14367080900648516, -1.0402462503531962, -3.257483552703506, -1.7801367945199174, 0.0789840881388051, -0.8353921099200822, -0.00945298474171628]
+HOMING_TIME = 4.0
+
+STEP_SIZE_L = 0.2
+STEP_SIZE_A = np.pi / 4
+STEP_TIME = 0.01
+DEADBAND = 0.1
+
+class TrajectoryClient(object):
+
+    def __init__(self):
+        self.gripper = actionlib.SimpleActionClient(
+                '/gripper_controller/gripper_action',
+                GripperCommandAction)
+        self.gripper.wait_for_server()
+        self.publisher = rospy.Publisher(
+                '/arm_controller/cartesian_twist/command',
+                TwistStamped, queue_size=1)
+        self.publish_rate = rospy.Rate(1)
+        self.client = actionlib.SimpleActionClient(
+                '/arm_controller/follow_joint_trajectory',
+                FollowJointTrajectoryAction)
+        self.client.wait_for_server()
+        self.joint_names = ["shoulder_pan_joint",
+                "shoulder_lift_joint", "upperarm_roll_joint",
+                "elbow_flex_joint", "forearm_roll_joint",
+                "wrist_flex_joint", "wrist_roll_joint"]
+
+    def send(self, twist, time):
+        xdot = TwistStamped()
+        xdot.twist.linear.x = twist[0]
+        xdot.twist.linear.y = twist[1]
+        xdot.twist.linear.z = twist[2]
+        xdot.twist.angular.x = twist[3]
+        xdot.twist.angular.y = twist[4]
+        xdot.twist.angular.z = twist[5]
+        xdot.header.stamp = rospy.Time.now()
+        xdot.header.frame_id = "shoulder_pan_joint"
+        self.publisher.publish(xdot)
+        rospy.sleep(time)
+
+    def send_joint(self, position, time):
+        waypoint = JointTrajectoryPoint()
+        waypoint.positions = position
+        waypoint.time_from_start = rospy.Duration(time)
+        goal = FollowJointTrajectoryGoal()
+        goal.trajectory.joint_names = self.joint_names
+        goal.trajectory.points.append(waypoint)
+        goal.trajectory.header.stamp = rospy.Time.now()
+        self.client.send_goal(goal)
+        rospy.sleep(time)
+
+    def open_gripper(self):
+        command = GripperCommand
+        command.max_effort = 60
+        command.position = 0.1
+        waypoint = GripperCommandGoal
+        waypoint.command = command
+        self.gripper.send_goal(waypoint)
+
+    def close_gripper(self):
+        command = GripperCommand
+        command.max_effort = 60
+        command.position = 0.0
+        waypoint = GripperCommandGoal
+        waypoint.command = command
+        self.gripper.send_goal(waypoint)
+
+
+class JointStateListener(object):
+
+  def __init__(self):
+    self.position = [0]*7
+    rospy.Subscriber("/joint_states", JointState, self.recorder)
+
+  def recorder(self, msg):
+    currtime = msg.header.stamp
+    position = msg.position
+    if len(position) > 10:
+        self.position = position[6:13]
+
+
+class Joystick(object):
+
+    def __init__(self):
+        pygame.init()
+        self.gamepad = pygame.joystick.Joystick(0)
+        self.gamepad.init()
+        self.deadband = 0.1
+
+    def input(self):
+        pygame.event.get()
+        A_pressed = self.gamepad.get_button(0)
+        return A_pressed
+
+
+def connect2robot(PORT):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind(('172.16.0.3', PORT))
+    s.listen(10)
+    conn, addr = s.accept()
+    return conn
+
+def send2robot(conn, qdot, limit=1.0):
+    qdot = np.asarray(qdot)
+    scale = np.linalg.norm(qdot)
+    if scale > limit:
+        qdot = np.asarray([qdot[i] * limit/scale for i in range(7)])
+    send_msg = np.array2string(qdot, precision=5, separator=',',suppress_small=True)[1:-1]
+    send_msg = "s," + send_msg + ","
+    conn.send(send_msg.encode())
+
+def listen2robot(conn):
+    state_length = 7 + 7 + 7 + 42
+    message = str(conn.recv(2048))[2:-2]
+    state_str = list(message.split(","))
+    for idx in range(len(state_str)):
+        if state_str[idx] == "s":
+            state_str = state_str[idx+1:idx+1+state_length]
+            break
+    try:
+        state_vector = [float(item) for item in state_str]
+    except ValueError:
+        return None
+    if len(state_vector) is not state_length:
+        return None
+    state_vector = np.asarray(state_vector)
+    state = {}
+    state["q"] = state_vector[0:7]
+    state["dq"] = state_vector[7:14]
+    state["tau"] = state_vector[14:21]
+    state["J"] = state_vector[21:].reshape((7,6)).T
+    return state
+
+def readState(conn):
+    while True:
+        state = listen2robot(conn)
+        if state is not None:
+            break
+    return state
+
+def xdot2qdot(xdot, state):
+    J_pinv = np.linalg.pinv(state["J"])
+    return np.matmul(J_pinv, np.asarray(xdot))
+
+
+def joint2pose(q):
+    def RotX(q):
+        return np.array([[1, 0, 0, 0], [0, np.cos(q), -np.sin(q), 0], [0, np.sin(q), np.cos(q), 0], [0, 0, 0, 1]])
+    def RotZ(q):
+        return np.array([[np.cos(q), -np.sin(q), 0, 0], [np.sin(q), np.cos(q), 0, 0], [0, 0, 1, 0], [0, 0, 0, 1]])
+    def TransX(q, x, y, z):
+        return np.array([[1, 0, 0, x], [0, np.cos(q), -np.sin(q), y], [0, np.sin(q), np.cos(q), z], [0, 0, 0, 1]])
+    def TransZ(q, x, y, z):
+        return np.array([[np.cos(q), -np.sin(q), 0, x], [np.sin(q), np.cos(q), 0, y], [0, 0, 1, z], [0, 0, 0, 1]])
+    H1 = TransZ(q[0], 0, 0, 0.333)
+    H2 = np.dot(RotX(-np.pi/2), RotZ(q[1]))
+    H3 = np.dot(TransX(np.pi/2, 0, -0.316, 0), RotZ(q[2]))
+    H4 = np.dot(TransX(np.pi/2, 0.0825, 0, 0), RotZ(q[3]))
+    H5 = np.dot(TransX(-np.pi/2, -0.0825, 0.384, 0), RotZ(q[4]))
+    H6 = np.dot(RotX(np.pi/2), RotZ(q[5]))
+    H7 = np.dot(TransX(np.pi/2, 0.088, 0, 0), RotZ(q[6]))
+    H_panda_hand = TransZ(-np.pi/4, 0, 0, 0.2105)
+    H = np.linalg.multi_dot([H1, H2, H3, H4, H5, H6, H7, H_panda_hand])
+    #print(H)
+    #print(H[:,3][:3])
+    return H[:,3][:3]
+
+def main():
+
+    PORT_robot = 8080
+    action_scale = 0.1
+
+    print('[*] Connecting to Fetch...')
+
+    rospy.init_node("endeffector_teleop")
+    mover = TrajectoryClient()
+    listener = JointStateListener()
+
+    print('[*] Connecting to Panda...')
+
+    conn = connect2robot(PORT_robot)
+    interface = Joystick()
+
+    print('[*] Ready for a teleoperation...')
+
+    run = False
+
+    while True:
+
+        state_panda = readState(conn)
+        state_fetch = list(listener.position)
+
+        A_pressed = interface.input()
+        if A_pressed:
+            print("[*] Running now!")
+            print(state_panda, state_fetch)
+
+        if run:
+            mover.open_gripper()
+            mover.send_joint(HOME_POSITION, HOMING_TIME)
+
+        action_panda = [0] * 6
+        action_fetch = [0] * 6
+        send2robot(conn, xdot2qdot(action_panda, state_panda))
+        mover.send(action_fetch, STEP_TIME)
+
+
+if __name__ == "__main__":
+    try:
+        main()
+    except rospy.ROSInterruptException:
+        pass
