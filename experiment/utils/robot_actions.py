2c4b3b8265e0a18fc8735ee791e94db8824e8d79
soheil-h <habibian@vt.edu>
2021-09-01 21:58:43 -0400

    angles

--- experiment/utils/robot_actions.py
+++ experiment/utils/robot_actions.py
@@ -6,14 +6,16 @@ def actionSpace():
     r = 0.05
     single_action_1 = []
     single_action_2 = []
-    angles = np.linspace(-np.pi/2, np.pi/2, n_actions)
-    for angle in angles:
-        action_1 = [r * np.cos(angle), r * np.sin(angle)]
-        action_2 = [-r * np.cos(angle), r * np.sin(angle)]
-        single_action_1.append(action_1)
-        single_action_2.append(action_2)
-    single_action_1.append([0,0])
-    single_action_2.append([0,0])
+    angles_h = np.linspace(-np.pi/2, np.pi/2, n_actions)
+    angles_v = np.linspace(0, -np.pi/2, n_actions)
+    for angle_h in angles_h:
+        for angle_v in angles_v:
+            action_1 = [r * np.cos(angle_h), r * np.sin(angle_h), r * np.sin(angle_v)]
+            action_2 = [-r * np.cos(angle_h), r * np.sin(angle_h), r * np.sin(angle_v)]
+            single_action_1.append(action_1)
+            single_action_2.append(action_2)
+    single_action_1.append([0,0,0])
+    single_action_2.append([0,0,0])
     A = []
     for idx in range(n_actions+1):
         for jdx in range(n_actions+1):
